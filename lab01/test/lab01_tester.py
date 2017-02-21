12b80b47bc129f388f294b77a728099f93cf2810
eleftheriabella <1996riabella@gmail.com>
2017-02-22 00:20:11 +0200

    Hope I completed lab01

--- lab01/test/lab01_tester.py
+++ lab01/test/lab01_tester.py
@@ -2,18 +2,18 @@
 import os.path
 from mipsTester import runTests
 
-if os.path.isfile("/usr/home/grads/courses/myy402/bin/MarsMYY402_4_5.jar"):
+if os.path.isfile("/usr/home/grads/courses/myy402/bin/MarsMYY402_4_5"):
   # This is the path to MarsMYY402_4_5.jar for the lab machines.
-  marsJar = "/usr/home/grads/courses/myy402/bin/MarsMYY402_4_5.jar"
+  marsJar = "C:\\Users\\User\\Downloads\\MarsMYY402_4_5.jar"
 else:
   ##########################################
   # This is my set-up for my machine,
   # MODIFY FOR YOUR PERSONAL COMPUTER SETUP!
   #  USING THE FULL PATH TO MarsMYY402_4_5.jar IN YOUR COMPUTER
   ##########################################
-  marsJar = "/home/efthym/t/ca_course/MarsMYY402_4_5.jar"
+  marsJar = "C:\\Users\\User\\Downloads\\MarsMYY402_4_5.jar"
 
-matric = 1000
+matric = 3039
 tests = [ \
  ("Matric simple test",\
   [('^matric:.*$', 'matric: .word %d' %(matric)) ],\
